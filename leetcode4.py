aa377829c45909d7caf74cb8eef71fbf2f3a1bcf
AUGUSTIN CURINIER <augustin.curinier@gmail.com>
2025-01-01 09:58:43 +0100

    better binary tree search

--- leetcode4.py
+++ leetcode4.py
@@ -26,7 +26,7 @@ class Solution:
         return median
 
     """https://leetcode.com/problems/median-of-two-sorted-arrays/editorial """
-    # merge and sort approach: get the k-th (k+1 if m+n is even) smallest integer of the merged lists --> median
+    # merge and sort approach: get the k-th (k+1 if m+n is even) smallest integer of the merged lists --> median, where k
     # do not need to consider the elements after the median since the 2 arrays are already sorted
     # time complexity: O(n + m) Space complexity: O(1).
     # Runtime: 5 ms,  beats 24%. Memory: 18.3 MB, beats 8 %
@@ -58,10 +58,10 @@ class Solution:
         if (m + n) % 2 == 0:
             for _ in range((m + n) // 2 - 1):
                 _ = get_min()
-            # (m + n) nth and (m+n+ 1) smallest element/2
+            # average of (m + n)/2 nth and (m+n)/2 + 1 smallest element
             return (get_min() + get_min()) / 2
 
-        # m + n is odd, so we repeat the search m+n//2 + 1 times to get the median element
+        # m + n is odd, median is (m+n+1)/2 th smallest element
         else:
             for _ in range((m + n) // 2):
                 _ = get_min()
@@ -79,6 +79,7 @@ class Solution:
             na, nb = len(A), len(B)
             n = na + nb
 
+            # k depends on whether total length na + nb is odd or even
             def solve(k, a_start, a_end, b_start, b_end):
                 # If the segment of on array is empty, it means we have passed all
                 # its element, just return the corresponding element in the other array.
@@ -104,8 +105,11 @@ class Solution:
                     else:
                         return solve(k, a_start, a_end, b_start, b_index - 1)
 
+            # total length is odd, k = n //2, same as n/2 + 1 th element bc index starts at 0 in Python
             if n % 2:
                 return solve(n // 2, 0, na - 1, 0, nb - 1)
+            # total length is even, average k-th and k+1-th elements where k = n//2 - 1
+            # same as averaging n/2th and n/2 + 1 - th elements, bc index start at 0 in Python
             else:
                 return (
                         solve(n // 2 - 1, 0, na - 1, 0, nb - 1)
@@ -114,10 +118,56 @@ class Solution:
 
 
     # Better binary search: performing the binary search only on the smaller array of nums1 and nums2, thus the time complexity is reduced to O(log(min(m,n))).
-    #
+    # Time complexity: O(log(m + n)) Space complexity:
     @staticmethod
     def binSearch2( A: List[int], B: List[int]) -> float:
-        pass
+
+        n, m = len(A), len(B)
+
+        # if B is the smaller array, swap the arrays
+        if m < n:
+            A,B = B,A
+            n, m = m,n
+
+        def solve(left, right):
+            partitionA = (left + right) //2
+            partitionB = (m+ n + 1)/2 - partitionA
+            # by construction, the smaller half partitionA + partitionB contains (m+n+1)/2 elements
+            # remember that the target value index depends on whether m+n is odd or even
+            # odd: find (m+m+1)/2 = (m + n) //2 + 1 th smallest element even: average (m+n)/2 and (m+n)/2 + 1 elements
+            maxLeftA = A[partitionA - 1]
+            minRightB = B[partitionB]
+
+            while left < right:
+
+                if maxLeftA > minRightB:
+                    # maxLeftA is too large to be in the smaller half,  we should look for a smaller partition value of A
+                    right = partitionA
+                else:
+                    left = partitionA
+
+                partitionA = (left + right) //2
+                partitionB = (m+ n + 1)/2 - partitionA
+                maxLeftA = A[partitionA - 1]
+                minRightB = B[partitionB]
+
+            minRightA = A[partitionA]
+            maxLeftB = B[partitionB - 1]
+
+            return maxLeftA, minRightA, maxLeftB, minRightB
+
+        maxLeftA, minRightA, maxLeftB, minRightB = solve(0, n)
+        # odd case, we search for (m + n)//2 + 1 th element
+        if (n+m) % 2:
+            # median is the max value of the smaller half
+            return max(maxLeftA, maxLeftB)
+
+        # even case, average (m + n)/2 and  (m + n)/2 + 1 elements
+        else:
+            # average max value of smaller half and min value of larger half
+            return (max(maxLeftA, maxLeftB) + min(minRightB, minRightA)) /2
+
+
 if __name__ == '__main__':
 
     nums1 = [1, 3]
